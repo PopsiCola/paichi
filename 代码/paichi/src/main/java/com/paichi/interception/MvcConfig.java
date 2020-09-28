690d032030810183fabdd03851f7296285b1e147
Workaholic <liulebinn@163.com>
2020-09-28 21:00:34 +0800

    添加拦截器，暂时未拦截任何请求，以及使用addviewController()实现找回密码页面无业务逻辑跳转。

---
+++ 代码/paichi/src/main/java/com/paichi/interception/MvcConfig.java
@@ -0,0 +1,49 @@
+package com.paichi.interception;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截的页面和请求的映射
+ * @Author liulebin
+ * @Date 2020/9/28 20:43
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+
+            /**
+             * 暂时无需拦截
+             * @param registry
+             */
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //InterceptorRegistration interceptorRegistration = registry.addInterceptor(new LoginInterceptor());
+                //
+                //interceptorRegistration.excludePathPatterns("/**");
+                //interceptorRegistration.addPathPatterns("/**");
+                ////                interceptorRegistration.excludePathPatterns("/", "/login.html", "/login", "/login/**", "/css/**", "/js/**", "/lib/**", "/images/**", "/fonts/**", "/error");
+                //interceptorRegistration.excludePathPatterns("/", "/**/login.html", "/**/login", "/**/login/**", "/**/css/**", "/**/js/**", "/**/lib/**", "/**/images/**", "/**/fonts/**", "/**/error");
+            }
+
+            /**
+             * 直接可以访问静态页面，无需经过Controller映射
+             * 配置之后发送/login.html相当于在controller中return "login"
+             * @param registry
+             */
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/login.html").setViewName("/login/index");
+                //直接访问找回密码页面
+                registry.addViewController("/resetpwd.html").setViewName("/login/resetpwd");
+            }
+        };
+    }
+}
